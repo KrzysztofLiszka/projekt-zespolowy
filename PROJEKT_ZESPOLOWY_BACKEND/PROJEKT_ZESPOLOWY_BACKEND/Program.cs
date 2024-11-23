e4240dc41a7f767a528d13929d6d311cdb919ebd
KrzysztofLiszka <krzysztof.liszkaa@wp.pl>
2024-11-23 12:10:15 +0100

    add: EF

--- PROJEKT_ZESPOLOWY_BACKEND/PROJEKT_ZESPOLOWY_BACKEND/Program.cs
+++ PROJEKT_ZESPOLOWY_BACKEND/PROJEKT_ZESPOLOWY_BACKEND/Program.cs
@@ -1,8 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using PROJEKT_ZESPOLOWY_BACKEND.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowCredentials();
+    });
+});
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
