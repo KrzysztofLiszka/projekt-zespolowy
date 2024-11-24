c18d516d83165dd466ff0bcffbe57b135b96cd14
Kacper Krok <Kacper>
2024-11-24 21:36:19 +0100

    update - sidebar template added

--- PROJEKT_ZESPOŁOWY_FRONTEND/PROJEKT_ZESPOLOWY_FRONTEND/src/app/app.component.ts
+++ PROJEKT_ZESPOŁOWY_FRONTEND/PROJEKT_ZESPOLOWY_FRONTEND/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthModule } from './core/modules/auth/auth.module';
+import { SidebarComponent } from './core/shared/components/sidebar/sidebar.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, AuthModule],
+    imports: [RouterOutlet, AuthModule, SidebarComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
---
+++ PROJEKT_ZESPOŁOWY_FRONTEND/PROJEKT_ZESPOLOWY_FRONTEND/src/app/core/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+@Component({
+    selector: 'app-sidebar',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './sidebar.component.html',
+    styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+    isLoggedIn: boolean = false;
+
+    constructor(private router: Router) { }
+
+    ngOnInit(): void {
+        this.router.events
+            .pipe(filter(event => event instanceof NavigationEnd))
+            .subscribe(() => {
+                this.checkIfLoggedIn();
+            });
+
+        this.checkIfLoggedIn();
+    }
+
+    checkIfLoggedIn(): void {
+        console.log(this.router.url);
+        this.isLoggedIn = this.router.url.endsWith('/home');
+    }
+}
