47ab700856a578fbc58098b1bab2df0e2bcc0bc9
Kacper Krok <Kacper>
2024-11-24 21:15:12 +0100

    update

--- PROJEKT_ZESPOŁOWY_FRONTEND/PROJEKT_ZESPOLOWY_FRONTEND/src/app/app.routes.ts
+++ PROJEKT_ZESPOŁOWY_FRONTEND/PROJEKT_ZESPOLOWY_FRONTEND/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './core/modules/auth/login-page/login-page.component';
 import { RegisterPageComponent } from './core/modules/auth/register-page/register-page.component';
+import { HomePageComponent } from './core/modules/home/home-page/home-page.component';
 
 export const routes: Routes = [
     { path: '', component: LoginPageComponent },
     { path: 'register', component: RegisterPageComponent },
+    { path: 'home', component: HomePageComponent }
 ];
