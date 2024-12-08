03b9f2984fdd49c53b0403e6af00dad54b669c6f
Kacper Krok <Kacper>
2024-12-08 19:31:47 +0100

    update - homepage changed

--- PROJEKT_ZESPOŁOWY_FRONTEND/PROJEKT_ZESPOLOWY_FRONTEND/src/app/app.component.ts
+++ PROJEKT_ZESPOŁOWY_FRONTEND/PROJEKT_ZESPOLOWY_FRONTEND/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthModule } from './core/modules/auth/auth.module';
 import { SidebarComponent } from './core/shared/components/sidebar/sidebar.component';
+import { TopbarComponent } from "./core/shared/components/topbar/topbar.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, AuthModule, SidebarComponent],
+    imports: [RouterOutlet, AuthModule, SidebarComponent, TopbarComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
